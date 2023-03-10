8e6c97b749a9c336e9a444dd0603ccc06c9fc4ce
Vedant <vedantbisht111@gmail.com>
2023-03-10 22:35:08 +0530

    updated solution

--- LEETCODE/9-Palindrome Number.cpp
+++ LEETCODE/9-Palindrome Number.cpp
@@ -31,8 +31,12 @@ public:
     }
 };
 
-/*------------------------------------------------Explanation----------------------------------------*/
-The solution works by first checking if the number is negative. Negative numbers cannot be palindromes, so we return false if the number is negative.
+/*------------------------------------------------Explanation----------------------------------------
+
+The solution works by first checking if the number is negative. Negative numbers cannot be 
+palindromes, so we return false if the number is negative.
 Next, we create two variables: original and reversed. We set original equal to the input x so that we can compare it to the reversed version of x later. We set reversed equal to 0, which is the starting point for reversing x.
 We then use a while loop to reverse x. Inside the loop, we update reversed by multiplying it by 10 and adding the remainder of x divided by 10. We then update x by dividing it by 10 (which removes the last digit).
 Once the loop is finished, we compare original to reversed. If they are equal, then x is a palindrome and we return true. Otherwise, x is not a palindrome and we return false.
+
+*/
