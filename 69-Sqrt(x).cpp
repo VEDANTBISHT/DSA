f400293cd70860aa4232ea35e22ad4a15384804f
Vedant <vedantbisht111@gmail.com>
2023-03-29 20:42:31 +0530

    solved

---
+++ 69-Sqrt(x).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1)) {
+                return mid;
+            } else if (mid > x / mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+};
+
+
+
+
+
