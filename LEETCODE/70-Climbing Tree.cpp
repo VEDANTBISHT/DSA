80b9f3015c4f72e945ac468ab8d23d087f575770
Vedant <vedantbisht111@gmail.com>
2023-04-25 23:17:58 +0530

    updated solution

--- LEETCODE/70-Climbing Tree.cpp
+++ LEETCODE/70-Climbing Tree.cpp
@@ -22,3 +22,33 @@ public:
         return dp[n];
     }
 };
+
+
+/*solution 2 without runtime error
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            // There are only two ways to reach the first two steps
+            return n;
+        }
+        
+        // Initialize variables for the first and second steps
+        int prev1 = 1, prev2 = 2;
+        
+        // For each step from the third step to the nth step,
+        // calculate the number of ways to reach that step by adding the
+        // number of ways to reach the previous two steps
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            
+            // Update variables for the previous two steps
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        
+        // The total number of ways to reach the nth step is stored in prev2
+        return prev2;
+    }
+};
