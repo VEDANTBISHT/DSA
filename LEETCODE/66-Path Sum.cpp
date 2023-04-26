4a7feedaba3c7f24daceb7e8067f6956a5759c76
Vedant <vedantbisht111@gmail.com>
2023-04-26 23:52:24 +0530

    solved

---
+++ LEETCODE/66-Path Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {  // iterate through digits from right to left
+            if (digits[i] < 9) {  // if the digit is less than 9, increment it and return the result
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;  // if the digit is 9, set it to 0 and continue to the next digit
+        }
+        digits.insert(digits.begin(), 1);  // if all digits are 9s, add a 1 to the front of the vector
+        return digits;
+    }
+};
